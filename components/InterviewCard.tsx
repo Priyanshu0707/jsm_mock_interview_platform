ac9f1f2c91929d8ae84cc5623538a46e9687b35e
Priyanshu0707 <priyanshubansal07072003@gmail.com>
2025-04-05 08:02:42 +0530

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import { getRandomInterviewCover } from '@/lib/utils';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
-function InterviewCard({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) {
+function InterviewCard({id, userId, role, type, techstack, createdAt}: InterviewCardProps) {
     const feedback = null as Feedback | null;
     const normalizedType= /mix/gi.test(type) ? 'Mixed' : type; 
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
@@ -39,8 +39,8 @@ function InterviewCard({interviewId, userId, role, type, techstack, createdAt}: 
             <DisplayTechIcons techStack={techstack} />
 
             <Button className='btn-primary'>
-                <Link href={feedback? `/interview/${interviewId}/feedback`
-                :`/interview/${interviewId}`}/>
+                <Link href={feedback? `/interview/${id}/feedback`
+                :`/interview/${id}`}/>
 
                 {feedback ? 'Check Feedback' : 'View Interview'}
             </Button>
